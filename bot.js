4809938cd57272355e39af5051e984983bbca4b0
LordJax <40278484+LordJax@users.noreply.github.com>
2018-08-30 16:57:07 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,9 +26,7 @@ fs.readdir("./commands/", (err, files) => {
 
 
 
-client.on('ready', () => {
-	 message.author.send("Bot restarted");
-}
+
 
 client.on('warn', console.warn);
 client.on('error', console.error);
